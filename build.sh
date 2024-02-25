9e8d7cd435aa67fe1b27132c28888f5defd83972
sergiejL <sergiejL@users.noreply.github.com>
2024-02-25 12:56:03 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-#
+#!/bin/bash
+
+docker build docker -t marlin_firmware_builder
+docker run -v "$PWD:/app" --name marlin_firmware_builder --rm -it -v .:/code marlin_firmware_builder /bin/bash -c "platformio run -e STM32G0B1RE_btt"
